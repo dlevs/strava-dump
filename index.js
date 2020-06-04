de908bbac8328aa1972d7a31267ed6eed52fb1bc
Daniel Levett <dlevett@hotmail.co.uk>
2020-06-04 23:05:11 +0100

    Add 0.5km increase challenge

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from 'fs-extra'
 import { fromDir } from './lib/pathUtils.js'
 import { getRunningData } from './lib/api.js'
-import { createRunsCsv, createAthletesCsv } from './lib/csvCreators.js'
+import { createRunsCsv, createAthletesCsv, createHalfKmIncreaseCsv } from './lib/csvCreators.js'
 
 const fromCurrentDir = (...paths) => fromDir(import.meta.url, ...paths)
 
@@ -13,6 +13,8 @@ const main = async () => {
     createAthletesCsv(fromCurrentDir('./dist/athletesAll.csv'), runs),
     createAthletesCsv(fromCurrentDir('./dist/athletes4k.csv'), runs.filter(({ distance }) => distance >= 4000 )),
     createAthletesCsv(fromCurrentDir('./dist/athletes5k.csv'), runs.filter(({ distance }) => distance >= 5000 )),
+    createAthletesCsv(fromCurrentDir('./dist/athletes10k.csv'), runs.filter(({ distance }) => distance >= 10000 )),
+    createHalfKmIncreaseCsv(fromCurrentDir('./dist/halfKmIncrease.csv'), runs)
   ])
 }
 
